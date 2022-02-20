0eaad70a0c065c18c1fd0c32d34badb9f0662c35
Volodymyr Dorozhinsky <vdorozhinsky@gmail.com>
2022-02-20 19:09:22 +0100

    Add Test Project
    The test empty project is added
    to be used as a stub when creating new projects

---
+++ TestProject/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 13, 2017
+ *      Author: dorozhin
+ */
+
+#include <iostream>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Test App was started" << std::endl;
+    system("sleep 30");
+    
+    return 0;
+}
+
+
