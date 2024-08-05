4ede35b7c7225dcbe203930c1cf47508b541353a
Volodymyr Dorozhinsky <vdorozhinsky@gmail.com>
2024-08-05 22:59:41 +0200

    Update Test App code to do something interesting

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -6,15 +6,29 @@
  */
 
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Test App was started" << std::endl;
-    system("sleep 30");
-    
-    return 0;
+    std::cout << "Test App was started \a" << std::endl;
+
+    // App termination loop
+    for (int i = 10; i >= 0; i--)
+    {
+        int status = system("sleep 1");
+
+        if (0 != status)
+        {
+            std::cout << "ERROR: system call returned non-zero status = " << status << std::endl;
+            return status;
+        }
+
+    std::cout << "Test App is terminating in " << i << " seconds      " << std::endl;
+    }
+    std::cout << "Test App has terminated\a" << std::endl;
+
+    return EXIT_SUCCESS;
 }
 
 
