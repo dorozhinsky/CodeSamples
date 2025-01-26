68a115fb6358006add83daea34de23bc19e8921e
Volodymyr Dorozhinsky <vdorozhinsky@yahoo.com>
2025-01-26 12:24:27 +0100

    Improve web-server simulator script
    The script features were extended:
     - use /tmp directory to store response HTML file;
     - extend response data;
     - improve formatting.

--- scripts/webserver-simulator.sh
+++ scripts/webserver-simulator.sh
@@ -9,6 +9,7 @@
 set -euo pipefail
 
 LISTEN_PORT="8080"
+readonly RESPONSE_FILE="/tmp/webserver-simulator-response.html"
 
 if [ $# -lt 1 ]; then
     echo "WebServer simulator will use default port: "$LISTEN_PORT
@@ -18,10 +19,18 @@ else
 fi
 
 while true; do
-    echo "Generating simple response HTML file..."
-    echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Welcome to $(uname --all)</h1><h2>Previous s erver connection date: $(date)</h2></body></html>" > response.html
+    echo "Generating simple response HTML file at ${RESPONSE_FILE} ..."
+    echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n\
+        <html><head>\
+            <meta http-equiv=\"refresh\" content=\"30\">\
+        </head><body>\
+            <h1>Welcome to $(uname --all)</h1>\
+            <h2>Previous server connection date: $(date)</h2>\
+            <p>Server uptime: $(uptime)</p>\
+            <h2>Server load:</h2>\
+            <pre>$(top -b -n 1)</pre>\
+        </body></html>" > $RESPONSE_FILE
     echo "Waiting for the connection..."
-nc -l -p $LISTEN_PORT -q 1 < response.html  
+nc -l -p $LISTEN_PORT -q 1 < $RESPONSE_FILE
     echo "The response was sent"
-    rm response.html
 done
