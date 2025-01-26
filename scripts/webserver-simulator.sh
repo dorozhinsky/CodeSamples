c5ffe4dfc98b0ea01e6b663e9d849fca409cb840
Volodymyr Dorozhinsky <vdorozhinsky@yahoo.com>
2025-01-26 17:16:49 +0100

    Extend web-server simulator script functionality
     - Added periodic response update on the server side;
     - added handlers for SIGINT and SIGTERM signals;
     - Added timestamps to the log prints.

--- scripts/webserver-simulator.sh
+++ scripts/webserver-simulator.sh
@@ -11,26 +11,37 @@ set -euo pipefail
 LISTEN_PORT="8080"
 readonly RESPONSE_FILE="/tmp/webserver-simulator-response.html"
 
+trap "echo 'Got SIGINT, terminating...'; exit" SIGINT
+trap "echo 'Got SIGTERM, terminating...'; exit" SIGTERM
+
+echo "$(date +"%Y-%m-%d %H:%M:%S.%3N %Z") - Starting WebServer simulator..."
+
 if [ $# -lt 1 ]; then
-    echo "WebServer simulator will use default port: "$LISTEN_PORT
+    echo "$(date +"%Y-%m-%d %H:%M:%S.%3N %Z") - WebServer simulator will use default port: "$LISTEN_PORT
 else
     LISTEN_PORT=$1
-    echo "WebServer simulator will use port: "$LISTEN_PORT
+    echo "$(date +"%Y-%m-%d %H:%M:%S.%3N %Z") - WebServer simulator will use port: "$LISTEN_PORT
 fi
 
 while true; do
-    echo "Generating simple response HTML file at ${RESPONSE_FILE} ..."
+    echo "$(date +"%Y-%m-%d %H:%M:%S.%3N %Z") - Generating simple response HTML file at ${RESPONSE_FILE} ..."
     echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n\
         <html><head>\
-            <meta http-equiv=\"refresh\" content=\"30\">\
+            <meta http-equiv=\"refresh\" content=\"300\">\
         </head><body>\
             <h1>Welcome to $(uname --all)</h1>\
-            <h2>Previous server connection date: $(date)</h2>\
+            <h2>Last data update time: $(date +"%Y-%m-%d %H:%M:%S.%3N %Z")</h2>\
             <p>Server uptime: $(uptime)</p>\
             <h2>Server load:</h2>\
             <pre>$(top -b -n 1)</pre>\
         </body></html>" > $RESPONSE_FILE
-    echo "Waiting for the connection..."
-nc -l -p $LISTEN_PORT -q 1 < $RESPONSE_FILE
-    echo "The response was sent"
+    echo "$(date +"%Y-%m-%d %H:%M:%S.%3N %Z") - Waiting for the connection..."
+    set +e
+timeout 30 nc -l -p $LISTEN_PORT -q 1 < $RESPONSE_FILE
+    if [ $? -eq 0 ]; then
+        echo "$(date +"%Y-%m-%d %H:%M:%S.%3N %Z") - The response was sent"
+    else
+        echo "$(date +"%Y-%m-%d %H:%M:%S.%3N %Z") - The connection timed out or failed"
+    fi
+set -e          
 done
